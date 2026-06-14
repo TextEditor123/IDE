8b5bbc19e954f9b78b69a4c5631bcc257607ec14
Luthetus <luthetus@gmail.com>
2026-06-13 20:51:52 -0400

    "When you write a song what comes first the riff or the lyrics?"

--- src/main.cjs
+++ src/main.cjs
@@ -1682,6 +1682,8 @@ I'm a very socially anxious person.
 I am going to bed in 10 minutes.
 
 I'm gonna have to lay there trying to fall asleep while playing in my head what I just said over and over.
+
+"When you write a song what comes first the riff or the lyrics?"
 */
 
 /* sec0
